2ab27e49d586c045e8f4cde2b3d8c3ec5dd7c505
SoftCloudCr <softcloudcr25@gmail.com>
2025-03-23 23:04:03 -0600

    Se crea la clase server, bd, env, y se realizan pruebas de conexion, todo funciona con exito.

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const pool = require("./models/db");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+// Ruta de prueba
+app.get("/", (req, res) => {
+  res.send("Servidor backend corriendo 🎯");
+});
+
+// Ruta de prueba con PostgreSQL
+app.get("/api/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ success: true, time: result.rows[0].now });
+  } catch (err) {
+    console.error("Error al conectar con PostgreSQL", err);
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
