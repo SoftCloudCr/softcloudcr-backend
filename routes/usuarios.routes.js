3754ea3a248cd07579de06cb1f90be0fb3bc4e4b
SoftCloudCr <softcloudcr25@gmail.com>
2025-03-24 23:48:06 -0600

    Se eliminan metodos de CRUD de empleados que lo hacian de forma general.

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const {
-  obtenerUsuarios,
-  crearUsuario,
-  actualizarUsuario,
-  eliminarUsuario,
   cambiarClave,
 } = require("../controllers/usuarios.controller");
 
@@ -12,10 +8,7 @@ const { importarEmpleados } = require("../controllers/usuarios.controller");
 const { obtenerEmpleadosConFiltros } = require("../controllers/usuarios.controller");
 
 // Rutas principales del CRUD
-router.get("/", obtenerUsuarios);         // Obtener todos los usuarios
-router.post("/", crearUsuario);           // Crear un nuevo usuario
-router.put("/:id", actualizarUsuario);    // Actualizar un usuario
-router.delete("/:id", eliminarUsuario);   // Eliminar un usuario
+
 router.patch("/:id/clave", cambiarClave); // Cambiar solo la contraseña del usuario
 
 router.post("/importar-empleados", importarEmpleados); // Importa los empleados desde  CSV
